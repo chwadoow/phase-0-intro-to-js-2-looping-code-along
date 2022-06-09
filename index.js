0e27e21bbd29309d58c18f3632ac8bf8ac21a594
moses ngige <moses32ngigz@gmail.com>
2022-06-09 15:27:04 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(array,event){
+    const myArray = [];
+    for( let i = 0 ; i < array.length;i++){
+        myArray[i]=`Thank you, ${array[i]}, for the wonderful ${event} gift!`;}
+return myArray;
+    }
+console.log(writeCards(["moz","james"],"birthday"));
+
+function countDown(n){
+    while( n >=0) {
+        console.log(n);
+        n--;
+    }
+        
+}
+
